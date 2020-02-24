11449410bebfad3490c760f28960778b2b3683b5
David Ramalho <dramalho@gmail.com>
2020-02-24 11:39:54 +0000

    Sample with some exceptions

--- lib/appsignal/transmitter.rb
+++ lib/appsignal/transmitter.rb
@@ -39,6 +39,15 @@ module Appsignal
           "#{config[:endpoint]}/1/#{base_uri}"
         end
       @config = config
+
+      # Sample down requests to 1/100th of actual calls
+      @track_request = [
+        ENV["RAILS_ENV"] == "test",
+        [Marker::ACTION, AuthCheck::ACTION].include?(base_uri),
+        Random.rand(100) == 0
+      ].any?
+
+      puts @track_request
     end
 
     def uri
@@ -54,8 +63,7 @@ module Appsignal
     end
 
     def transmit(payload)
-      # Sample down requests to 1/100th of actual calls
-      return unless Random.rand(100) == 0
+      return unless @track_request
 
       config.logger.debug "Transmitting payload to #{uri}"
       http_client.request(http_post(payload))
